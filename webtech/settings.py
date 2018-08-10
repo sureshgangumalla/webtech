42f048e0e7b34eb3c62452684ed0f11a8498ed16
saimahesh59 <33434179+saimahesh59@users.noreply.github.com>
2018-08-10 20:52:50 -0300

    Update settings.py

--- webtech/settings.py
+++ webtech/settings.py
@@ -129,13 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
