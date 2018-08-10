58d18de5cbacc42020985ebc5eeac65a686a7aee
saimahesh59 <33434179+saimahesh59@users.noreply.github.com>
2018-08-10 19:36:26 -0300

    Update settings.py

--- webtech/settings.py
+++ webtech/settings.py
@@ -130,12 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
